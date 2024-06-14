61b91f87d25eaf3bf129490dcd776bfda4bc977b
Bowie <134542537+araujo-angel@users.noreply.github.com>
2024-06-14 10:00:53 -0300

    lista encadeada

--- src/main/java/dataStructure/LinkedList.java
+++ src/main/java/dataStructure/LinkedList.java
@@ -52,17 +52,113 @@ public class LinkedList {
     }
 
     public void print() {
+        System.out.println("################################");
         Node temp = this.head;
         while (temp != null){
             System.out.println(temp.data);
             temp = temp.next;
         }
+        System.out.println("################################");
+    }
+
+    public void append(String data){
+        Node newNode = new Node(data);
+        if (length == 0){
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast(){
+        if(length == 0) return null;
+        Node pre = head;
+        Node temp = null;
+        while(pre.next != tail) {
+            pre = pre.next;
+        }
+        temp = tail;
+        tail = pre;
+        tail.next = null;
+
+        length --;
+        if (length == 0){
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    public void prepend(String data){
+        Node newNode = new Node(data);
+        if (length == 0){
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length ++;
+    }
+
+    public Node removeFirst(){
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length --;
+        if(length == 0){
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index){
+        if(index < 0 || index >= length) return null;
+        Node temp = head;
+        for(int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean insert(int index, String data){
+        if(index < 0 || index > length) return false;
+        if(index == 0){
+            prepend(data);
+            return true;
+        }
+        if(index == length){
+            append(data);
+            return true;
+        }
+        Node newNode = new Node(data);
+        Node temp = get(index -1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length ++;
+        return true;
     }
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList("elemento 1");
-        list.getHead();
-        list.getTail();
-        list.getLentgh();
+        list.append("elemento 2");
+        list.append("elemento 3");
+        list.prepend("elemento 0");
+
+        list.insert(3, "elemento 2.5");
+        //System.out.println(list.get(2).data);
+       // System.out.println(list.removeFirst().data);
+        list.print();
+
+//        list.getHead();
+//        list.getTail();
+//        list.getLentgh();
+//        list.print();
     }
 }
